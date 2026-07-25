61f102bda689c652ff0b3ab007243841f6f8b22d
zhuravlova-k <katazuravlova391@gmail.com>
2026-07-25 18:51:45 +0300

    feat: add LongIntegers class implementation

---
+++ integers-v2/LongIntegers.cs
@@ -0,0 +1,64 @@
+namespace Literals;
+
+public static class LongIntegers
+{
+    public static long ReturnLongInteger21()
+    {
+        return 4_956_185_095_298_947_214L;
+    }
+
+    public static long ReturnLongInteger22()
+    {
+        return -1_280_010_762_458_239_942L;
+    }
+
+    public static long ReturnLongInteger23()
+    {
+        return -945_783_496_234_828_465L;
+    }
+
+    public static ulong ReturnLongInteger24()
+    {
+        return 16_269_823_234_523_742_845uL;
+    }
+
+    public static long ReturnLongInteger25()
+    {
+        return 0x7FFF_FFFF_FFFF_FFFFL;
+    }
+
+    public static long ReturnLongInteger26()
+    {
+        return 773_738_404_492_802_748L;
+    }
+
+    public static ulong ReturnLongInteger27()
+    {
+        return 17_977_307_477_258_691_517uL;
+    }
+
+    public static ulong ReturnLongInteger28()
+    {
+        return 14_193_065_825_095_688_383uL;
+    }
+
+    public static long ReturnLongInteger29()
+    {
+        return 0b00111000_11101000_11010101_00111010_10111010_01101010_10011010_10010101L;
+    }
+
+    public static long ReturnLongInteger210()
+    {
+        return 1_645_102_583_813_967_509L;
+    }
+
+    public static long ReturnLongInteger211()
+    {
+        return 6_148_914_691_236_517_205L;
+    }
+
+    public static long ReturnLongInteger212()
+    {
+        return 8_446_744_073_709_551_615L;
+    }
+}
