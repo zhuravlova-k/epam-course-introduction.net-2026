2129874034934f7ccc427669b336ac4530248fcf
zhuravlova-k <katazuravlova391@gmail.com>
2026-07-29 19:00:39 +0300

    feat: implement factorial, fibonacci and arithmetic sequences

---
+++ for-statements/ArithmeticSequences.cs
@@ -0,0 +1,43 @@
+﻿namespace ForStatements;
+
+public static class ArithmeticSequences
+{
+    public static int SumArithmeticSequenceTerms1(int a, int n)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += a + i;
+        }
+
+        return sum;
+    }
+
+    public static int SumArithmeticSequenceTerms2(int n)
+    {
+        const int firstTerm = 47;
+        const int commonDifference = 13;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += firstTerm + (i * commonDifference);
+        }
+
+        return sum;
+    }
+
+    public static int SumArithmeticSequenceTerms3(int a, int n)
+    {
+        const int commonDifference = 5;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += a + (i * commonDifference);
+        }
+
+        return sum;
+    }
+}
---
+++ for-statements/Factorial.cs
@@ -0,0 +1,34 @@
+﻿namespace ForStatements;
+
+public static class Factorial
+{
+    public static int GetFactorial(int n)
+    {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static int SumFactorialDigits(int n)
+    {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        int sum = 0;
+
+        for (; factorial > 0; factorial /= 10)
+        {
+            sum += factorial % 10;
+        }
+
+        return sum;
+    }
+}
