d3af77fd53d93baf01395443a90b0eb962d2ab69
zhuravlova-k <katazuravlova391@gmail.com>
2026-08-14 18:59:44 +0300

    feat: implement methods with in and out parameters

---
+++ out-ref-in-params/MethodsWithInParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace CreatingMethods;
+
+public static class MethodsWithInParameters
+{
+    public static bool ReturnDefaultValueWithoutChangingParameter(in bool boolValue) => false;
+
+    public static char ReturnDefaultValueWithoutChangingParameter(in char charValue) => '\0';
+
+    public static float ReturnDefaultValueWithoutChangingParameter(in float floatValue) => 0.0f;
+
+    public static int ReturnDefaultValueWithoutChangingParameter(in int intValue) => 0;
+
+    public static long ReturnDefaultValueWithoutChangingParameter(in long longValue) => 0L;
+}
---
+++ out-ref-in-params/MethodsWithOutParameters.cs
@@ -0,0 +1,34 @@
+﻿namespace CreatingMethods;
+
+public static class MethodsWithOutParameters
+{
+    public static void ReturnValues(out bool trueValue, out bool falseValue)
+    {
+        trueValue = true;
+        falseValue = false;
+    }
+
+    public static void ReturnValues(out char lowerCaseA, out char upperCaseA)
+    {
+        lowerCaseA = 'a';
+        upperCaseA = 'A';
+    }
+
+    public static void ReturnValues(out float minFloatValue, out float maxFloatValue)
+    {
+        minFloatValue = float.MinValue;
+        maxFloatValue = float.MaxValue;
+    }
+
+    public static void ReturnValues(out int minIntValue, out int maxIntValue)
+    {
+        minIntValue = int.MinValue;
+        maxIntValue = int.MaxValue;
+    }
+
+    public static void ReturnValues(out long minLongValue, out long maxLongValue)
+    {
+        minLongValue = long.MinValue;
+        maxLongValue = long.MaxValue;
+    }
+}
