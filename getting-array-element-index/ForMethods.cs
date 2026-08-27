92957878f5a0f59fef0a49553faa52674443d681
zhuravlova-k <katazuravlova391@gmail.com>
2026-08-27 19:09:49 +0300

    feat: implement array search using for and while loops

---
+++ getting-array-element-index/ForMethods.cs
@@ -0,0 +1,107 @@
+﻿namespace GettingArrayElementIndex
+{
+    public static class ForMethods
+    {
+        public static int GetIndexOf(uint[]? arrayToSearch, uint value)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            for (int i = 0; i < arrayToSearch.Length; i++)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int GetIndexOf(uint[]? arrayToSearch, uint value, int startIndex, int count)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            int lastIndex = startIndex + count;
+
+            for (int i = startIndex; i < lastIndex; i++)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(uint[]? arrayToSearch, uint value)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            for (int i = arrayToSearch.Length - 1; i >= 0; i--)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(uint[]? arrayToSearch, uint value, int startIndex, int count)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            if (startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            for (int i = startIndex + count - 1; i >= startIndex; i--)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ getting-array-element-index/WhileMethods.cs
@@ -0,0 +1,119 @@
+﻿namespace GettingArrayElementIndex
+{
+    public static class WhileMethods
+    {
+        public static int GetIndexOf(ushort[]? arrayToSearch, ushort value)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            int i = 0;
+            while (i < arrayToSearch.Length)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+
+            return -1;
+        }
+
+        public static int GetIndexOf(ushort[]? arrayToSearch, ushort value, int startIndex, int count)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            int lastPosition = startIndex + count;
+            if (lastPosition > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            int i = startIndex;
+            while (i < lastPosition)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(ushort[]? arrayToSearch, ushort value)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            int i = arrayToSearch.Length - 1;
+            while (i >= 0)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+
+                i--;
+            }
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(ushort[]? arrayToSearch, ushort value, int startIndex, int count)
+        {
+            ArgumentNullException.ThrowIfNull(arrayToSearch);
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            int lastPosition = startIndex + count;
+            if (lastPosition > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            int i = lastPosition - 1;
+            while (i >= startIndex)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+
+                i--;
+            }
+
+            return -1;
+        }
+    }
+}
