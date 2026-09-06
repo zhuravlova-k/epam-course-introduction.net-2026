c9f670813bba1fc95bbccb2e7faf60366ccb0bb0
zhuravlova-k <katazuravlova391@gmail.com>
2026-09-06 22:53:45 +0300

    feat: implement string literals tasks

---
+++ strings/StringLiterals.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace Strings;
+
+public static class StringLiterals
+{
+    /// <summary>
+    /// Returns a "abc" string.
+    /// </summary>
+    public static string ReturnAbcStringLiteral()
+    {
+        return "abc";
+    }
+
+    /// <summary>
+    /// Returns a "ABCDEF" string.
+    /// </summary>
+    public static string ReturnAbcdefStringLiteral()
+    {
+        return "ABCDEF";
+    }
+
+    /// <summary>
+    /// Returns an empty string.
+    /// </summary>
+    public static string ReturnEmptyString()
+    {
+        return string.Empty;
+    }
+
+    /// <summary>
+    /// Returns a file path.
+    /// </summary>
+    public static string ReturnFilePathStringLiteral1()
+    {
+        return "c:\\documents\\files\\myfile0234.txt";
+    }
+
+    /// <summary>
+    /// Returns a file path.
+    /// </summary>
+    public static string ReturnFilePathStringLiteral2()
+    {
+        return "c:\\users\\myuser\\documents\\my-video-file.avi";
+    }
+
+    /// <summary>
+    /// Returns a file path.
+    /// </summary>
+    public static string ReturnFilePathVerbatimStringLiteral1()
+    {
+        return @"c:\documents\files\datafile0582.txt";
+    }
+
+    /// <summary>
+    /// Returns a file path.
+    /// </summary>
+    public static string ReturnFilePathVerbatimStringLiteral2()
+    {
+        return @"c:\users\myuser\documents\my-audio-file.mp3";
+    }
+}
