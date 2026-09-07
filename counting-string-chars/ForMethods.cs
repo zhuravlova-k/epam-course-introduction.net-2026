7b4b5b5e94de6d0d86be6922c8eabb784703eb63
zhuravlova-k <katazuravlova391@gmail.com>
2026-09-07 19:07:05 +0300

    feat: implement iterative and recursive char counting

---
+++ counting-string-chars/ForMethods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CountingStringChars;
+
+public static class ForMethods
+{
+    public static int GetCharCount(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        int count = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    public static int GetUpperCharCount(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        int count = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsUpper(str[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int GetCharCountRecursive(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        return GetCharCountRecursive(str, 0);
+    }
+
+    public static int GetUpperCharCountRecursive(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        return GetUpperCharCountRecursive(str, 0);
+    }
+
+    private static int GetCharCountRecursive(string str, int index)
+    {
+        if (index >= str.Length)
+        {
+            return 0;
+        }
+
+        return GetCharCountRecursive(str, index + 1) + 1;
+    }
+
+    private static int GetUpperCharCountRecursive(string str, int index)
+    {
+        if (index >= str.Length)
+        {
+            return 0;
+        }
+
+        bool isUpper = char.IsUpper(str[index]);
+        int currentIncrement = isUpper ? 1 : 0;
+
+        return GetUpperCharCountRecursive(str, index + 1) + currentIncrement;
+    }
+}
---
+++ counting-string-chars/WhileMethods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace CountingStringChars;
+
+public static class WhileMethods
+{
+    public static int GetSpaceCount(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        int count = 0;
+        int i = 0;
+
+        while (i < str.Length)
+        {
+            if (char.IsWhiteSpace(str[i]))
+            {
+                count++;
+            }
+
+            i++;
+        }
+
+        return count;
+    }
+
+    public static int GetPunctuationCount(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        int count = 0;
+        int i = 0;
+
+        while (i < str.Length)
+        {
+            if (char.IsPunctuation(str[i]))
+            {
+                count++;
+            }
+
+            i++;
+        }
+
+        return count;
+    }
+
+    public static int GetSpaceCountRecursive(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        if (string.IsNullOrEmpty(str))
+        {
+            return 0;
+        }
+
+        int result = GetSpaceCountRecursive(str[1..]) + (char.IsWhiteSpace(str[0]) ? 1 : 0);
+
+        return result;
+    }
+
+    public static int GetPunctuationCountRecursive(string? str)
+    {
+        ArgumentNullException.ThrowIfNull(str);
+
+        if (string.IsNullOrEmpty(str))
+        {
+            return 0;
+        }
+
+        bool isPunctuation = char.IsPunctuation(str[0]);
+        int currentIncrement = isPunctuation ? 1 : 0;
+        int result = GetPunctuationCountRecursive(str[1..]) + currentIncrement;
+
+        return result;
+    }
+}
