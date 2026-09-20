92af4bf5448ee49ffca1f17e3291ea4a0f0cbc90
zhuravlova-k <katazuravlova391@gmail.com>
2026-09-20 19:01:32 +0300

    feat: implement object creation with named arguments and initializers

---
+++ uml-design-basics/CreatingSimpleClass16.cs
@@ -0,0 +1,29 @@
+namespace UmlDesignBasics;
+
+public static class CreatingSimpleClass16
+{
+    public static object CreateSimpleClass16WithDefaultValues()
+    {
+        return new SimpleClass16();
+    }
+
+    public static object CreateSimpleClass16(long longValue)
+    {
+        return new SimpleClass16(longValue: longValue);
+    }
+
+    public static object CreateSimpleClass16(float floatValue)
+    {
+        return new SimpleClass16(floatValue: floatValue);
+    }
+
+    public static object CreateSimpleClass16(int intValue, bool boolValue)
+    {
+        return new SimpleClass16(intValue: intValue, boolValue: boolValue);
+    }
+
+    public static object CreateSimpleClass16(double doubleValue, char charValue, string stringValue)
+    {
+        return new SimpleClass16(doubleValue: doubleValue, charValue: charValue, stringValue: stringValue);
+    }
+}
---
+++ uml-design-basics/CreatingSimpleClass2.cs
@@ -0,0 +1,25 @@
+namespace UmlDesignBasics;
+
+public static class CreatingSimpleClass2
+{
+    public static object CreateSimpleClass2()
+    {
+        SimpleClass2 simpleClass = new SimpleClass2();
+        simpleClass.IntField = -331;
+        simpleClass.FloatField = 13.13f;
+        simpleClass.DoubleField = -31.31;
+        simpleClass.BooleanField = true;
+        return simpleClass;
+    }
+
+    public static object CreateSimpleClass2ObjectInitializer()
+    {
+        return new SimpleClass2
+        {
+            IntField = 432,
+            FloatField = -42.31f,
+            DoubleField = 43.12,
+            BooleanField = false,
+        };
+    }
+}
