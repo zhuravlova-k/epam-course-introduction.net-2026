e174765e2b099727d9944b1051774678dda6bc6a
zhuravlova-k <katazuravlova391@gmail.com>
2026-09-20 19:02:53 +0300

    feat: implement object creation via factory method

---
+++ uml-design-basics/CreatingSimpleClass15.cs
@@ -0,0 +1,29 @@
+namespace UmlDesignBasics;
+
+public static class CreatingSimpleClass15
+{
+    public static object CreateSimpleClass15WithDefaultValues()
+    {
+        return SimpleClass15.Create();
+    }
+
+    public static object CreateSimpleClass15(double doubleValue)
+    {
+        return SimpleClass15.Create(doubleValue: doubleValue);
+    }
+
+    public static object CreateSimpleClass15(int intValue)
+    {
+        return SimpleClass15.Create(intValue: intValue);
+    }
+
+    public static object CreateSimpleClass15(long longValue, char charValue)
+    {
+        return SimpleClass15.Create(longValue: longValue, charValue: charValue);
+    }
+
+    public static object CreateSimpleClass15(float floatValue, bool boolValue, object objectValue)
+    {
+        return SimpleClass15.Create(floatValue: floatValue, boolValue: boolValue, objectValue: objectValue);
+    }
+}
